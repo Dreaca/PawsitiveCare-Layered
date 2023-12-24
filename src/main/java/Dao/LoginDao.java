496eae87783c74199472e0e738ff2630e8f3c6ef
dreaca <buddhikadreaca@gmail.com>
2023-12-24 13:29:39 +0530

    Login Dao done

--- src/main/java/Dao/LoginDao.java
+++ src/main/java/Dao/LoginDao.java
@@ -1,4 +1,20 @@
 package Dao;
 
+import Dto.AdminDto;
+import Dto.LoginFormDto;
+
+import java.sql.SQLException;
+
 public interface LoginDao {
+     String generateNExtUserID() throws SQLException;
+    String splitUserID(String userId);
+    boolean authenticate(LoginFormDto login) throws SQLException;
+    String getUser(LoginFormDto login) throws SQLException;
+    boolean deleteUser(String userId) throws SQLException;
+    boolean setUpdateUserId(String oldUserId, String newUserId) throws SQLException;
+    AdminDto getUser(String userName) throws SQLException;
+    boolean saveUser(LoginFormDto LDto) throws SQLException;
+    boolean updateUserName(String userName, String userIdText) throws SQLException;
+    boolean checkPass(String userId, String oldPasswordText) throws SQLException;
+    boolean updatePassword(String userId, String newpW) throws SQLException;
 }
